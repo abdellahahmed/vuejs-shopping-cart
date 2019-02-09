0ff89c596829940959169e409e0a9beeb9708764
Abdellah Ahmed <24692961+abdellahahmed@users.noreply.github.com>
2019-02-09 12:40:11 +0600

    updated product image and info

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product:'Socks'
+        product:'Socks',
+        image: 'images/sock_green.jpeg',
+        inStock: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "/images/sock_green.jpeg"
+            },
+            {   
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "/images/sock_blue.jpeg"
+            }
+        ],
+
+        cart: 0,
+
+
+
+     //data end   
+    },
+
+    methods: {
+        addToCart: function () {
+            this.cart += 1;
+        },
+        updateProduct: function (variantImage) {
+            this.image = variantImage; 
+        }
     }
 })
