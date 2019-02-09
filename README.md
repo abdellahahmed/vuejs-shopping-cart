f7c37d46b56a5580b1c5a5995604267e724ccfd8
Abdellah Ahmed <24692961+abdellahahmed@users.noreply.github.com>
2019-02-09 14:12:32 +0600

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Shopping Cart with Vue.js
+
+Introduction:
+Simple shopping cart using Vue.js
+This is an introduction to Vuejs and its properties on the Vue instances.
+
+References: 
+Vuejs Documentation -> https://vuejs.org/v2/guide/
+Vue Mastery -> https://www.vuemastery.com/
+
+
+
--- main.js
+++ main.js
@@ -1,37 +1,50 @@
 let app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product:'Socks',
-        image: 'images/sock_green.jpeg',
-        inStock: false,
+        selectedVariant: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
 
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: "/images/sock_green.jpeg"
+                variantImage: "/images/sock_green.jpeg",
+                variantQuantity: 10
             },
             {   
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: "/images/sock_blue.jpeg"
+                variantImage: "/images/sock_blue.jpeg",
+                variantQuantity: 5
             }
         ],
 
-        cart: 0,
-
-
-
-     //data end   
+        cart: 0,  
     },
+    //end of data
 
     methods: {
-        addToCart: function () {
+        addToCart() {
             this.cart += 1;
         },
-        updateProduct: function (variantImage) {
-            this.image = variantImage; 
+        updateProduct(index) {
+            this.selectedVariant = index; 
+        }
+    },
+    // end of method
+
+    computed: { 
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
         }
     }
+    // end of computed
 })
